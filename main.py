0adbc0db46cca2aa453d61edf69e6ec885f3cea7
JernejL123 <jernej.lobnik@gmail.com>
2026-04-14 20:19:42 +0200

    Sorgotovo stevilo

--- main.py
+++ main.py
@@ -16,7 +16,16 @@ df = pd.read_csv(a,header=None)
 df.columns = ['0', 'ds', 'y', '3']
 df['ds'] = pd.to_datetime(df['ds'] + ' 2024', format='%d.%m %H:%M %Y') #doda datum kr drugace panda ne dela
 
-print(df.head())
+
+brki = np.quantile(df['y'], [0,0.25,0.5,0.75,1])
+low_high_AVG = brki[3] - brki[1]
+cb_fac = 0.1
+
+sorgo_num = low_high_AVG * cb_fac
+print("Brki: ", brki)
+print("Šorgotovo število: ", sorgo_num)
+
+#print(df.head())
 m = Prophet(changepoint_range=0.8, changepoint_prior_scale=0.95)
 m.add_seasonality(name='hourly', period=0.04, fourier_order=20)
 m.fit(df)
@@ -25,7 +34,7 @@ future_period = 10
 future_period_freq ='h'
 future = m.make_future_dataframe(periods=future_period, freq=future_period_freq)
 forecast = m.predict(future)
-print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+#print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
 forecast_df = forecast[['ds','yhat','yhat_upper','yhat_lower']]
 forecast_df
@@ -40,7 +49,7 @@ forecasting_final['uncertainty'] = forecasting_final['yhat_upper'] - forecasting
 
 # We this factor we can identify the outlier or anomaly. 
 # This factor can be customized based on the data
-factor = 1.5
+factor = 0.5
 forecasting_final['anomaly'] = forecasting_final.apply(lambda x: 'Yes' 
       if(np.abs(x['error']) >  factor*x['uncertainty']) else 'No', axis = 1)
 
@@ -49,5 +58,5 @@ fig = px.scatter(forecasting_final, x='ds', y='y', color='anomaly', title='Anoma
                  color_discrete_map=color_discrete_map)
 
 #fig = m.plot(forecasting_final)
-fig.show()
-fig.waitforbuttonpress()
+#fig.show()
+#fig.waitforbuttonpress()
